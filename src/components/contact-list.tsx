89d97f45c2ab8d91b43c83b5e53391816e8c3f7c
Arnold MBANZA <20ma304@esisalama.org>
2025-03-10 03:24:38 +0100

    J'ai corrigé quelques erreurs dans le fichier contact-list.tsx

--- src/components/contact-list.tsx
+++ src/components/contact-list.tsx
@@ -5,9 +5,10 @@ import ContactCard from './contact-card';
 
 export default function ContactList() {
   const { contacts } = use(ContactContext);
+
   return (
     <div className='w-full'>
-      {contacts.length === 0 && <h5>Il n'y a plus de contact</h5>}
+      {contacts.length === 0 && <h5>Il n&apos;y a plus de contact</h5>}
       {contacts.map((contact) => (
         <ContactCard contact={contact} key={contact.id} />
       ))}
